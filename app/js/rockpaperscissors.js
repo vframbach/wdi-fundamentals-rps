ecb9c1f09cfec76b4ff247d67b30a993bd40db1a
Victoria Frambach <vframbach@gmail.com>
2015-10-17 19:31:40 -0700

    updated computer and player actions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,8 +40,20 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+    if (playerMove == computerMove)
+        return "tie";
+    if (playerMove == "rock" && computerMove == "paper")
+        return "computer";
+    if (playerMove == "paper" && computerMove == "scissors")
+        return "computer";
+    if (playerMove == "scissors" && computerMove == "rock")
+        return "computer";
+    if (playerMove == "scissors" && computerMove == "paper")
+        return "player";
+    if (playerMove == "paper" && computerMove == "rock")
+        return "player";
+    if (playerMove == "rock" && computerMove == "scissors")
+        return "player";
 }
 
 function playToFive() {
